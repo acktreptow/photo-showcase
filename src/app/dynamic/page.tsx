f835de5aa3bc5ef0fc3179e330d70ee71b4bad45
acktreptow <acktreptow@gmail.com>
2024-06-12 19:47:28 +0100

    add dynamic/static routes

---
+++ src/app/dynamic/page.tsx
@@ -0,0 +1,8 @@
+function DynamicPage() {
+  return (
+    <div>
+      <h1>Dynamic Page</h1>
+      <p>This is the dynamic page</p>
+    </div>
+  );
+}
---
+++ src/app/static/page.tsx
@@ -0,0 +1,10 @@
+function StaticPage() {
+  return (
+    <div>
+      <h1>Static Page</h1>
+      <p>This is the static page</p>
+    </div>
+  );
+}
+
+export default StaticPage;
