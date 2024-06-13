b2dbd243e556316d6d24b9fcd1f249f365fc2dbe
acktreptow <acktreptow@gmail.com>
2024-06-13 20:22:00 +0100

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 function Homepage() {
   return (
     <main className="container mx-auto flex-grow">
-      <h1>Photo showcase homepage</h1>
+      <h1 className="text-2xl text-center p-10">Photo showcase homepage</h1>
+      <p className="text-center">Showcasing dynamic and static rendering</p>
     </main>
   );
 }
