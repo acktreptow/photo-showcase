eeec2a3a3cf7537156b0778b4eeee07f2ca4ba53
acktreptow <acktreptow@gmail.com>
2024-06-14 19:43:17 +0100

    add image to static page

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,15 +1,28 @@
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Static Rendering",
   description: "Showcasing static rendering in Next.js through photos",
 };
 
-function StaticPage() {
+async function StaticPage() {
+  const res = await fetch(
+    "https://api.unsplash.com/photos/random?client_id=" +
+      process.env.UNSPLASH_ACCESS_KEY
+  );
+  const image = await res.json();
+
   return (
     <div className="container mx-auto flex-grow">
       <h1>Static Page</h1>
       <p>This is the static page</p>
+      <Image
+        src={image.urls.regular}
+        alt={image.alt_description}
+        width={image.width}
+        height={image.height}
+      />
     </div>
   );
 }
