8fea5300b720880f2669b75f2354f2ca3cb63bab
acktreptow <acktreptow@gmail.com>
2024-06-15 23:11:20 +0100

    add all required photo infortmation

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Static Rendering",
@@ -29,17 +30,30 @@ async function StaticPage() {
         height={250}
         className="mx-auto rounded-lg shadow-lg mb-5"
       />
+      <div className="text-center text-lg cursor mb-5 text-sky-500 font-semibold underline">
+        <Link href={image.links.download} target="_blank" className="text-lg">
+          <span>Download Original Photo</span>
+        </Link>
+      </div>
       <div className="border-2 border-gray-200 rounded-lg shadow-md p-4">
-        <h2 className="text-center font-semibold text-xl mb-3">
+        <h2 className="text-center font-bold text-xl mb-3">
           Photo Information
         </h2>
-        <p className="text-lg">Name: {image.user.name}</p>
+        <p className="text-lg">
+          Taken by: {image.user.name} ({image.user.username})
+        </p>
         <p className="text-lg">
           Description:{" "}
           {image.description === null ? "None provided." : image.description}
         </p>
         <p className="text-lg">
           Location taken: {image.location.city}, {image.location.country}
+        </p>
+        <p className="text-lg">
+          Portfolio:{" "}
+          <Link href={image.user.portfolio_url} target="_blank">
+            {image.user.portfolio_url}
+          </Link>
         </p>
       </div>
     </div>
