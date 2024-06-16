44924dbc153c884164effbc0466a6507a4fe6f0e
acktreptow <acktreptow@gmail.com>
2024-06-16 22:17:39 +0100

    fill out dynamic page

--- src/app/dynamic/page.tsx
+++ src/app/dynamic/page.tsx
@@ -1,15 +1,102 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Dynamic Rendering",
   description: "Showcasing dynamic rendering in Next.js through photos",
 };
 
-function DynamicPage() {
+type UnsplashImage = {
+  alt_description: string;
+  description: string;
+  links: {
+    download: string;
+  };
+  location: {
+    city: string;
+    country: string;
+  };
+  urls: {
+    regular: string;
+  };
+  user: {
+    name: string;
+    username: string;
+    portfolio_url: string;
+  };
+};
+
+async function DynamicPage() {
+  const res = await fetch(
+    "https://api.unsplash.com/photos/random?client_id=" +
+      process.env.UNSPLASH_ACCESS_KEY,
+    { cache: "no-cache" }
+  );
+  const image: UnsplashImage = await res.json();
+
+  let locationData = "";
+  if (image.location.city === null && image.location.country === null) {
+    locationData = "Unknown";
+  } else if (image.location.city !== null && image.location.country === null) {
+    locationData = image.location.city;
+  } else if (image.location.city === null && image.location.country !== null) {
+    locationData = image.location.country;
+  } else {
+    locationData = `${image.location.city}, ${image.location.country}`;
+  }
+
   return (
-    <div className="container mx-auto flex-grow">
-      <h1>Dynamic Page</h1>
-      <p>This is the dynamic page</p>
+    <div className="container mx-auto bg-white flex-grow p-4">
+      <h1 className="text-2xl text-center font-bold mt-4 md:mt-3 mb-5 lg:text-4xl lg:mt-8 lg:mb-10">
+        Dynamic Page
+      </h1>
+      <p className="bg-teal-700 rounded-lg text-zinc-100 p-3 mb-7 text-lg md:w-3/4 lg:p-5 lg:mb-10  md:mx-auto">
+        This is the <span className="font-semibold">dynamic page</span> because
+        it doesn&#39;t cache the image that is fetched from the Unsplash API.
+        You as the user will therefore always see a new image with every
+        refresh.
+      </p>
+      <Image
+        src={image.urls.regular}
+        alt={image.alt_description}
+        width={256}
+        height={512}
+        className="mx-auto rounded-lg shadow-lg mb-5 w-64 md:w-80 lg:w-96"
+      />
+      <div className="text-center text-lg cursor mb-5 text-sky-500 font-semibold underline lg:mb-10">
+        <Link href={image.links.download} target="_blank" className="text-lg">
+          <span>Download Original Photo</span>
+        </Link>
+      </div>
+      <div className="border-2 border-gray-200 rounded-lg shadow-md p-4 md:w-3/4 md:mx-auto md:text-center">
+        <h2 className="text-center font-bold text-xl mb-3">
+          Photo Information
+        </h2>
+        <p className="text-lg">
+          Taken By: <span className="capitalize">{image.user.name}</span> (
+          {image.user.username})
+        </p>
+        <p className="text-lg">
+          Description:{" "}
+          {image.description === null
+            ? image.alt_description.charAt(0).toUpperCase() +
+              image.alt_description.slice(1)
+            : image.description.charAt(0).toUpperCase() +
+              image.description.slice(1)}
+        </p>
+        <p className="text-lg">Location Taken: {locationData}</p>
+        <p className="text-lg">
+          Portfolio:{" "}
+          {image.user.portfolio_url ? (
+            <Link href={image.user.portfolio_url} target="_blank">
+              {image.user.portfolio_url}
+            </Link>
+          ) : (
+            "None Provided"
+          )}
+        </p>
+      </div>
     </div>
   );
 }
