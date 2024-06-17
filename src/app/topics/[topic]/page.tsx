54ddeda67e7fbaf8759455f94752767ba1a30b80
acktreptow <acktreptow@gmail.com>
2024-06-17 22:35:12 +0100

    implement topic fetch call

--- src/app/topics/[topic]/page.tsx
+++ src/app/topics/[topic]/page.tsx
@@ -1,12 +1,30 @@
+import Image from "next/image";
+import { UnsplashImage } from "../../types/UnsplashImage";
+
 async function TopicPage({ params: { topic } }: any) {
   const res = await fetch(
-    `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    `https://api.unsplash.com/photos/random?query=${topic}&count=12&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
   );
 
+  const images = await res.json();
+
   return (
-    <div>
-      <h1>Topic Page</h1>
-      {topic}
+    <div className="container mx-auto bg-white flex-grow p-4">
+      <h1 className="text-2xl text-center font-bold mt-4 md:mt-3 mb-5 lg:text-4xl lg:mt-8 lg:mb-10 capitalize">
+        {topic}
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 border">
+        {images.map((image: any) => (
+          <Image
+            src={image.urls.regular}
+            width={200}
+            height={200}
+            alt={image.description}
+            key={image.urls.raw}
+            className="w-full h-full object-cover"
+          />
+        ))}
+      </div>
     </div>
   );
 }
