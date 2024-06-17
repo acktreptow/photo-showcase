ef2da99ebaec6afc147e508dd6c30fb5cb07e981
acktreptow <acktreptow@gmail.com>
2024-06-17 15:32:20 +0100

    add homepage photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import homepageImg from "../../public/images/homepage-photo.png";
+
 function Homepage() {
   return (
-    <main className="container mx-auto flex-grow">
-      <h1 className="text-2xl text-center p-10">Photo showcase homepage</h1>
-      <p className="text-center">Showcasing dynamic and static rendering</p>
+    <main className="bg-white container mx-auto flex-grow p-4">
+      <h1 className="text-4xl text-center my-10 font-bold">Photo Showcase</h1>
+      <p className="bg-teal-700 text-center rounded-lg text-zinc-100 p-3 mb-5 text-lg md:w-3/4 lg:p-5 lg:mb-10 md:mx-auto">
+        Photo Showcase is a Next.js application that fetches and caches data
+        differently depending on the page you visit. There is both static and
+        dynamic data fetching. The dynamic page fetches data at runtime, while
+        the static page fetches data at build time.
+      </p>
+      <Image
+        src={homepageImg}
+        alt="A man taking photos in nature"
+        className="rounded-lg md:w-3/4 mx-auto shadow-lg mb-10"
+      />
+      <p className="font-semibold text-center text-2xl">Enjoy the showcase!</p>
     </main>
   );
 }
