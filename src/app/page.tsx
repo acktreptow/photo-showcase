2b3a5dfa8a39f1c641ccf5c11d5766f2d1fcbaed
acktreptow <acktreptow@gmail.com>
2024-06-17 19:44:52 +0100

    update image placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,35 +21,40 @@ function Homepage() {
 
   return (
     <main className="bg-white container mx-auto flex-grow p-4">
-      <h1 className="text-4xl text-center my-10 font-bold">Photo Showcase</h1>
-      <p className="bg-teal-700 text-center rounded-lg text-zinc-100 p-3 mb-5 text-lg md:w-3/4 lg:p-5 lg:mb-10 md:mx-auto">
+      <h1 className="text-4xl font-bold mt-5 mb-10 text-center lg:my-10 lg:text-6xl">
+        Photo Showcase
+      </h1>
+      <p className="bg-teal-700 text-center rounded-lg text-zinc-100 p-3 mb-10 text-lg md:w-3/4 lg:p-5 md:mx-auto">
         Photo Showcase is a Next.js application that fetches and caches data
         differently depending on the page you visit. There is both static and
         dynamic data fetching. The dynamic page fetches data at runtime, while
         the static page fetches data at build time.
       </p>
-      <Image
-        src={homepageImg}
-        alt="A man taking photos in nature"
-        className="rounded-lg md:w-3/4 mx-auto shadow-lg mb-10"
-      />
-      <p className="font-semibold text-center text-2xl mb-5">
-        Enjoy the showcase!
-      </p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="text-xl mb-10 lg:tracking-wide">
         <select
           value={selectedPage}
           onChange={(e) => setSelectedPage(e.target.value)}
+          className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-3 hover:border-playstation transition-colors duration-200 text-gray-800 shadow-md focus:outline-none"
         >
-          <option>Pick a Page</option>
+          <option value="">Select Showcase</option>
           {pages.map((page) => (
             <option key={page.name} value={page.urlPath}>
               {page.name}
             </option>
           ))}
         </select>
-        <button type="submit">Go to Page</button>
+        <button
+          type="submit"
+          className="block mx-auto border p-4 mb-5 bg-teal-700 text-white rounded-full hover:bg-teal-900 transition-colors duration-200 shadow-md shadow-teal-700"
+        >
+          Go To Selected Showcase
+        </button>
       </form>
+      <Image
+        src={homepageImg}
+        alt="A man taking photos in nature"
+        className="rounded-lg md:w-3/4 mx-auto shadow-lg"
+      />
     </main>
   );
 }
