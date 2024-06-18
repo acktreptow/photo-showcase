7db03f06e7dd121859283141b6e823405cacab9c
acktreptow <acktreptow@gmail.com>
2024-06-18 17:17:31 +0100

    update css

--- src/app/topics/[topic]/page.tsx
+++ src/app/topics/[topic]/page.tsx
@@ -27,16 +27,20 @@ async function TopicPage({
       <h1 className="text-2xl text-center font-bold mt-4 md:mt-3 mb-5 lg:text-4xl lg:mt-8 lg:mb-10 capitalize">
         {topic}
       </h1>
-      <p className="bg-teal-700 rounded-lg text-zinc-100 p-3 mb-7 text-lg md:w-3/4 lg:p-5 lg:mb-10  md:mx-auto">
-        This is the <span className="font-semibold">topics page</span> because
-        it uses the topic parameter to fetch images from the Unsplash API. While
-        the dog images are static because they were fetched and cached at build
-        time, other topics will be dynamic. <br />
-        Replace dogs with another topic in the URL to see images of that topic.
-        These will be fetched and rendered on first access and then cached for
-        subsequent visits (the photos will not change on refresh).
-      </p>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 border">
+      <div className="bg-teal-700 rounded-lg text-zinc-100 p-3 mb-7 text-lg md:w-3/4 lg:p-5 lg:mb-10  md:mx-auto">
+        <p className="mb-5">
+          This is the <span className="font-semibold">topics page</span> because
+          it uses the topic parameter to fetch images from the Unsplash API.
+          While the dog images are static because they were fetched and cached
+          at build time, other topics will be dynamic.
+        </p>
+        <p>
+          Replace dogs with another topic in the URL to see images of that
+          topic. These will be fetched and rendered on first access and then
+          cached for subsequent visits (the photos will not change on refresh).
+        </p>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 md:p-5 lg:p-10">
         {images.map((image: UnsplashImage) => (
           <Image
             src={image.urls.regular}
