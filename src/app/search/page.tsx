d9b30b4254845abf66ee76f41eb27fac4c3601b5
acktreptow <acktreptow@gmail.com>
2024-06-21 23:49:21 +0100

    create search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,21 @@
+import Container from "../components/Container";
+import Title from "../components/Title";
+import TextBox from "../components/TextBox";
+
+function SearchPage() {
+  return (
+    <Container>
+      <Title title="Search" />
+      <TextBox>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
+          corporis non! Tempora, distinctio? Minus nihil aspernatur enim
+          adipisci hic, quas iure laboriosam fuga tempore impedit, animi porro
+          dolore accusamus? Voluptatum.
+        </p>
+      </TextBox>
+    </Container>
+  );
+}
+
+export default SearchPage;
