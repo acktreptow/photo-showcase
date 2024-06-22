5f9cb69934e02a54600f699134f7df3f7be773da
acktreptow <acktreptow@gmail.com>
2024-06-22 22:59:07 +0100

    add type safety

--- src/app/api/search/route.tsx
+++ src/app/api/search/route.tsx
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
+import { UnsplashImage } from "@/app/types/UnsplashImage";
 
-export async function GET(request: Request) {
+type UnsplashSearchResponse = {
+  results: UnsplashImage[];
+};
+
+export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const searchQuery = searchParams.get("searchQuery");
 
@@ -15,7 +20,7 @@ export async function GET(request: Request) {
     `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
   );
 
-  const { results } = await res.json();
+  const { results }: UnsplashSearchResponse = await res.json();
 
   return NextResponse.json({ results });
 }
