82c139d2c8a957b92a76540db72699a5ddda1803
acktreptow <acktreptow@gmail.com>
2024-06-22 23:41:19 +0100

    add search results jsx

--- src/app/search/page.client.tsx
+++ src/app/search/page.client.tsx
@@ -5,6 +5,7 @@ import TextBox from "../components/TextBox";
 import Title from "../components/Title";
 import { useState } from "react";
 import { UnsplashImage } from "../types/UnsplashImage";
+import Image from "next/image";
 
 function SearchPageClient() {
   const [searchResults, setSearchResults] = useState<UnsplashImage[] | null>(
@@ -52,10 +53,39 @@ function SearchPageClient() {
           placeholder="puppies, pizza etc"
           className="border"
         />
-        <button type="submit" className="border">
+        <button
+          type="submit"
+          className="border"
+          disabled={searchResultsLoading}
+        >
           Search
         </button>
       </form>
+
+      <div>
+        {searchResultsLoading && <p>Loading...</p>}
+        {searchResultsError && (
+          <p>There was an error. Please try your query again.</p>
+        )}
+        {searchResults?.length === 0 && (
+          <p>No results found. Please try a different query.</p>
+        )}
+      </div>
+
+      {searchResults && (
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 md:p-5 lg:p-10">
+          {searchResults.slice(0, 6).map((image: UnsplashImage) => (
+            <Image
+              src={image.urls.regular}
+              width={200}
+              height={200}
+              alt={image.alt_description}
+              key={image.urls.raw}
+              className="w-full h-full object-cover rounded-lg"
+            />
+          ))}
+        </div>
+      )}
     </Container>
   );
 }
