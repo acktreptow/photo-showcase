6b20b674f7129de620ff3bb832111c54cb8c594e
acktreptow <acktreptow@gmail.com>
2024-06-23 22:10:14 +0100

    incorporate linksdata

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../../public/images/logo.png";
+import linksData from "../data/links.json";
 
 function Header(): JSX.Element {
-  interface LinkI {
-    name: string;
-    urlPath: string;
-  }
-
-  const links: LinkI[] = [
-    { name: "Home", urlPath: "/" },
-    { name: "Static", urlPath: "/static" },
-    { name: "Dynamic", urlPath: "/dynamic" },
-    { name: "ISR", urlPath: "/isr" },
-    { name: "Dogs", urlPath: "/topics/dogs" },
-    { name: "Search", urlPath: "/search" },
-  ];
-
   return (
     <header className="container mx-auto p-4 bg-teal-700 text-zinc-100 flex justify-between items-center">
       <div>
@@ -30,7 +17,7 @@ function Header(): JSX.Element {
       </div>
       <nav>
         <ul className="flex space-x-3 font-semibold text-lg md:space-x-4 md:mr-5 lg:space-x-5  lg:mr-10 lg:text-xl">
-          {links.map((link) => (
+          {linksData.map((link) => (
             <li
               key={link.name}
               className={`hover:text-zinc-300 hover:underline ${
